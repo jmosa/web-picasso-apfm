ca5ec4e44916f10e54d6386f35543a8366251ab8
Marshall Owen <marshallo@aplaceformom.com>
2020-04-30 14:41:41 -0500

    remove multisite

--- html/wp-config.php
+++ html/wp-config.php
@@ -116,13 +116,13 @@ define('FS_METHOD', 'direct');
 define( 'DISALLOW_FILE_EDIT', true );
 
 /* Multisite */
-define('WP_ALLOW_MULTISITE', true);
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'web-picasso.'.$GLOBALS['environment'].'.aplaceformom.com');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+// define('WP_ALLOW_MULTISITE', true);
+// define('MULTISITE', true);
+// define('SUBDOMAIN_INSTALL', false);
+// define('DOMAIN_CURRENT_SITE', 'web-picasso.'.$GLOBALS['environment'].'.aplaceformom.com');
+// define('PATH_CURRENT_SITE', '/');
+// define('SITE_ID_CURRENT_SITE', 1);
+// define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy publishing. */
 
